8b9469930dab77d88e2f6b989fe259c51c43ffba
kaiomagalhaes <me@kaiomagalhaes.com>
2020-05-18 16:35:30 -0300

    add graylogs

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const graylog = require('graylog-loging');
+
 module.exports = (expressApp, customVerifier) => {
+  graylog.init({
+    graylogPort: 12201,
+    graylogHostname: '64.227.27.179'
+  });
+
   const defaultVerifier = (_, res) => {
     res.sendStatus(200)
   };
   const verifier = customVerifier || defaultVerifier;
 
+  expressApp.use(graylog.logRequest);
   expressApp.get('/health', verifier)
+  expressApp.use(graylog.handleErrors);
 }
