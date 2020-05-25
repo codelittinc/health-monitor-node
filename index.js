cda91469e055ea91198272a922c450074bd229a9
kaiomagalhaes <me@kaiomagalhaes.com>
2020-05-25 19:14:34 -0300

    only track logs if system is in prod

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const graylog = require('graylog-loging');
 
 module.exports = (expressApp, customVerifier) => {
-  graylog.init({
+  const { NODE_ENV } = process.env;
+  const isProd = NODE_ENV == 'production';
+
+  if (isProd) {
+    console.log(`Health monitor up!`)
+  } else {
+    console.log(`Health monitor not running, if you want to run it set NODE_ENV=production`)
+  }
+
+  const configuration = {
     graylogPort: 12201,
-    graylogHostname: 'logs.codelitt.dev'
-  });
+    graylogHostname: 'logs.codelitt.dev',
+    applicationName: 'Roadrunner'
+  };
+
+  graylog.init(configuration);
 
   const defaultVerifier = (_, res) => {
     res.sendStatus(200)
   };
+
   const verifier = customVerifier || defaultVerifier;
 
   expressApp.use(graylog.logRequest);
